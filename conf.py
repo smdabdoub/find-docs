e88e5c77d0ff25138d1f8967b573d203e09cbf72
Shareef Dabdoub <shareef@dabdoub.net>
2010-11-03 09:03:28 +0000

    The latex output now uses parts instead of chapters.

--- conf.py
+++ conf.py
@@ -189,7 +189,7 @@ latex_logo = 'find_blue_final.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-latex_use_parts = True
+latex_use_parts = False
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
